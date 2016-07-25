a0857e4423d28d12df915011085343c23c2b5f10
eronoobos <eronoobos@gmail.com>
2016-07-24 20:38:27 -0700

    fix defendbehaviour Name

--- defendbehaviour.lua
+++ defendbehaviour.lua
@@ -14,11 +14,6 @@ local CMD_PATROL = 15
 local CMD_MOVE_STATE = 50
 local MOVESTATE_ROAM = 2
 
-
-function DefendBehaviour:Name()
-	return "DefendBehaviour"
-end
-
 -- not does it defend, but is it a dedicated defender
 function IsDefender(unit)
 	return defenderList[unit:Internal():Name()] or false
